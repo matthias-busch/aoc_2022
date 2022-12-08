5bc5c11d8223363f83b14b5b3323de92ddc7e7ed
Matthias Busch (Datadrivers GmbH) <matthias.busch@datadrivers.de>
2022-12-09 00:36:05 +0100

    day 8 done

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Solutions to this years [Advent of Code](https://adventofcode.com/2022).
 - Day 04: ** | 2022-12-04
 - Day 05: ** | 2022-12-05
 - Day 06: ** | 2022-12-06
+- Day 07: ...
+- Day 08: ** | 2022-12-09
--- src/08_day.py
+++ src/08_day.py
@@ -9,16 +9,6 @@ raw_input = read_input_file('08')
 
 lines = raw_input.splitlines()
 
-test_input="""30373
-25512
-65332
-33549
-35390
-"""
-
-test_lines = test_input.splitlines()
-
-
 def count_visible_trees(tree_map: list[list[int]]) -> int:
 
     visible_trees = []
@@ -48,9 +38,94 @@ def count_visible_trees(tree_map: list[list[int]]) -> int:
     return len(visible_trees)
 
 
+def compute_scenic_score(x: int, y: int, tree_map: list[list[int]]) -> dict[int]:
+    left_distance : int = 0
+    right_distance : int = 0
+    up_distance : int = 0
+    down_distance : int = 0
+    # print(f'{x = } {y = } {tree_map[x][y] = }')
+
+    # check top border
+    if x == 0:
+        up_distance += 0
+
+    # check bottom border
+    elif x == len(tree_map) - 1:
+        down_distance += 0
+
+    # check left border
+    elif y == 0:
+        left_distance += 0
+
+    # check right border
+    elif y == len(tree_map[x]) - 1:
+        right_distance += 0
+
+    # check inner trees
+    else:
+        # check left
+        left = tree_map[x][:y]
+        for tree in reversed(left):
+            if tree_map[x][y] > tree:
+                left_distance += 1
+            elif tree_map[x][y] == tree:
+                left_distance += 1
+                break
+            else:
+                left_distance += 0
+                break
+
+        # check right
+        right = tree_map[x][y+1:]
+        for tree in right:
+            if tree_map[x][y] > tree:
+                right_distance += 1
+            elif tree_map[x][y] == tree:
+                right_distance += 0
+                break
+            else:
+                right_distance += 1
+                break
+
+        # check up
+        up = [row[y] for row in tree_map[:x]]
+        for tree in reversed(up):
+            if tree_map[x][y] > tree:
+                up_distance += 1
+            elif tree_map[x][y] == tree:
+                up_distance += 1
+                break
+            else:
+                up_distance += 0
+                break
+
+        # check down
+        down = [row[y] for row in tree_map[x+1:]]
+        for tree in down:
+            if tree_map[x][y] > tree:
+                down_distance += 1
+            elif tree_map[x][y] == tree:
+                down_distance += 1
+                break
+            else:
+                down_distance += 0
+                break
+    # print(f'{left_distance = } {right_distance = } {up_distance = } {down_distance = }')
+    scenic_score = left_distance * right_distance * up_distance * down_distance
+
+    return scenic_score
+
+scenic_scores = []
+for x in range(len(lines)):
+        for y in range(len(lines[x])):
+            scenic_scores.append(compute_scenic_score(x=x, y=y, tree_map=lines))
+
+
 # Results
 solution_1 = count_visible_trees(tree_map=lines)
 print(f'{solution_1 = }')
 
+solution_2 = max(scenic_scores)
+print(f'{solution_2 = }')
 
 
